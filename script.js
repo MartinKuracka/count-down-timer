ba33e54c4d8d3864c41f0145a0bbcd3c1fe6969f
martin kuracka <m.kuracka@gmail.com>
2021-03-05 11:29:56 +0100

    adding hours and minutes to countdown

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
 const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
-
+const timeEl = document.getElementById('time-picker');
 const countdownEl = document.getElementById('countdown');
 const countdownElTitle = document.getElementById('countdown-title');
 const countdownBtn = document.getElementById('countdown-button');
 const timeElements = document.querySelectorAll('span');
 
-let countdownTitle = '';
-let countdownDate = '';
-let countdownValue = Date;
-let countdownActive;
+var countdownTitle = '';
+var countdownDate = '';
+var countdownTime = '';
+var countdownValue = Date;
+var formPickedDate = Date;
+var countdownTimePicked;
+var formSetDate
+var countdownActive;
+var timeNow;
+var distandceTimeEl;
+var timeToAddToCountdown;
+var now;
 
 const second = 1000;
 const minute = second*60;
 const hour = minute*60;
 const day = hour*24;
 
-// Count down function
-let countdownRunning = () => {
-    setInterval(updateDOM(), 1000)
-}
+// set the date and time input minimum with values from NOW
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
+setInterval(() => {
+    // Get the current time value in miliseconds
+    timeNow = String(new Date).substring(16,21);
+    // console.log(timeNow);    
+    distandceTimeEl = timeNow.substring(0,2)*3600000 + timeNow.substring(3,5)*60000; 
+    // console.log(distandceTimeEl);
+}, 2000);
+
 // populate Countdown / Complete UI
 function updateDOM() {
     countdownActive = setInterval(() => {
-        const now = new Date().getTime();
-        const distance = countdownValue - now;
+        const now = new Date().getTime() + 3600000;
+        const distance = (countdownValue + countdownTimeMsValue) - now;
+        console.log(distance);
+        // console.log('countdown value = ',countdownValue, 'countdown miliseconds', countdownTimeMsValue);
+        // console.log('terz hodnota', now)
 
         const days = Math.floor(distance/day);
-        const hours = Math.floor((distance % day)/hour) - 1;
+        const hours = Math.floor((distance % day)/hour);
         const minutes = Math.floor((distance % hour)/minute);
         const seconds = Math.floor((distance % minute)/second);
 
@@ -45,22 +63,55 @@ function updateDOM() {
     }, 1000);
 }
 
-// set the date input minimum with today's date
-const today = new Date().toISOString().split('T')[0];
-dateEl.setAttribute('min', today);
+// Let me know if chage in form field happened
+const checkForm = (e) => {
+    console.log(e);
+    console.log(timeNow)
+    if (e.srcElement.attributes[0].ownerElement.id === "date-picker") {
+        formSetDate = e.srcElement.value;
+        // console.log(formSetDate);
+        // console.log(today)
+        if (formSetDate === today) {
+            timeEl.setAttribute('min', `${timeNow.substring(0,2)}:${Number(timeNow.substring(3))+1}`);
+        } else {
+            timeEl.removeAttribute('min')
+        }
+    }
+}
+
+
+// const timeNow = new Date().toISOString().tolocaleTimeString('eu').split('T')[1].substring(0,5);
+// console.log(new Date.tolocaleTimeString());
+timeEl.setAttribute('min', timeNow)
 
 function updateCountdown(e) {
     e.preventDefault();
+    console.log(e);
     countdownTitle = e.srcElement[0].value;
     countdownDate = e.srcElement[1].value;
+    countdownTime = e.srcElement[2].value;    
+    console.log(countdownDate,' + ', countdownTime);
     // Get number version of current date
     countdownValue = new Date(countdownDate).getTime();
-    console.log(countdownValue);
+    console.log('countdown value = ',countdownValue)
+    if (formSetDate === today) {
+        countdownTimeMsValue = (countdownTime.substring(0,2)*3600000 + countdownTime.substring(3,5)*60000) - distandceTimeEl;
+        now = new Date().getTime();
+        console.log('now time',now)
+        countdownValue = now + 3600000;
+        console.log('when date equals today', countdownValue);
+    } else {
+        countdownTimeMsValue = countdownTime.substring(0,2)*3600000 + countdownTime.substring(3,5)*60000;
+        const distance = (countdownValue + countdownTimeMsValue) - now;
+        // console.log('when date different from today', countdownTimeMsValue);
+    }
+    // console.log(countdownValue, countdownTime);
     updateDOM();
 }
 
 // Event lstener
 countdownForm.addEventListener('submit', updateCountdown);
+countdownForm.addEventListener('change', checkForm);
 countdownBtn.addEventListener('click', () => {
     clearInterval(countdownActive);
     //  Show input field
